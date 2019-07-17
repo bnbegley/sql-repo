1d410c3fe846ad4dccb80b0e818f374721794ddd
Brailee Begley <brail33na11@gmail.com>
2019-07-17 10:43:38 -0400

    Union Query Added

---
+++ Union-Query.sql
@@ -0,0 +1,12 @@
+Select Firstname, Lastname, Description, Salary, 'Yes' as 'SR. EXEC'
+from Job
+left join Employee
+	on Employee.jobid = Job.Id
+	where Description = 'President' or Description = 'VP'
+UNION
+	Select Firstname, Lastname, Description, Salary, 'No' as 'SR. EXEC'
+from Job
+left join Employee
+	on Employee.jobid = Job.Id
+	where NOT(Description = 'President' or Description = 'VP')
+	order by "SR. EXEC" Desc
